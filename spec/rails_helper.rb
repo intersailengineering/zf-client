bd15f227d6e41de8a4dc3ce1e9dd93651c13e55b
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2015-04-29 15:33:56 +0200

    removed db dependency

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,7 +4,7 @@ ENV["RAILS_ENV"] = "test"
 require 'rails/all'
 require 'rspec/rails'
 require 'spec_helper'
-require 'database_cleaner'
+# require 'database_cleaner'
 require 'factory_girl'
 require 'faker'
 require 'shoulda-matchers'
@@ -24,20 +24,23 @@ RSpec.configure do |config|
   # Activate factory girl
   config.include FactoryGirl::Syntax::Methods
 
+  ##
   # Database handling
+  ##
+
   # migrate test schema if needed
-  ActiveRecord::Migrator.migrate(File.join(Rails.root, 'db/migrate'))
+  # ActiveRecord::Migrator.migrate(File.join(Rails.root, 'db/migrate'))
 
   # Clean database at start of the suite and then
   # just uses transactions
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |single_test|
-    DatabaseCleaner.cleaning do
-      single_test.run
-    end
-  end
+  # config.before(:suite) do
+  #   DatabaseCleaner.strategy = :transaction
+  #   DatabaseCleaner.clean_with(:truncation)
+  # end
+  #
+  # config.around(:each) do |single_test|
+  #   DatabaseCleaner.cleaning do
+  #     single_test.run
+  #   end
+  # end
 end
--- zf_client.gemspec
+++ zf_client.gemspec
@@ -23,8 +23,7 @@ Gem::Specification.new do |s|
   s.add_dependency 'faker'
 
   # Databse connector
-  #@jtodoIMP remove the db dependency
-  s.add_dependency 'pg'
+  # s.add_dependency 'pg'
   s.add_dependency 'httparty'
 
   # Development
@@ -35,7 +34,7 @@ Gem::Specification.new do |s|
   # Automated tests with guard
   s.add_development_dependency 'guard-rspec'
   # Auto Clean DB
-  s.add_development_dependency 'database_cleaner'
+  # s.add_development_dependency 'database_cleaner'
   # More Rspec matches
   s.add_development_dependency 'shoulda-matchers'
   s.add_development_dependency 'shoulda-callback-matchers', '~> 1.1.1'
