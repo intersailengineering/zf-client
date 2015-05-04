195b5729c425c1d03b7867ae73662f23c5a5ceba
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2015-05-04 18:10:11 +0200

    back to user manager on client

--- app/services/intersail/zf_client/client/z_client.rb
+++ app/services/intersail/zf_client/client/z_client.rb
@@ -27,7 +27,7 @@ module Intersail
         # List of all the managers as name => class
         def delegated
           {
-              user: Intersail::ZfClient::Client::ZFakeUserManager,
+              user: Intersail::ZfClient::Client::ZUserManager,
               role: Intersail::ZfClient::Client::ZRoleManager,
               unit: Intersail::ZfClient::Client::ZUnitManager,
               urr: Intersail::ZfClient::Client::ZUrrManager,
