c792c4ece3c59ae34f43dbe3c86be819e536c858
jacopo-beschi-intersail <jacopo.beschi@intersail.it>
2015-05-14 16:22:18 +0200

    used eager load path

--- lib/zf_client/engine.rb
+++ lib/zf_client/engine.rb
@@ -2,6 +2,6 @@ module ZfClient
   class Engine < ::Rails::Engine
     isolate_namespace ZfClient
 
-    config.autoload_paths << File.expand_path("#{config.root}/lib", __FILE__)
+    config.eager_load_paths << File.expand_path("#{config.root}/lib", __FILE__)
   end
 end
